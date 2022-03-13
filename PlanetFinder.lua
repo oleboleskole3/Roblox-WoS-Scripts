238a5211eedd8042ef1f33e0d8039c1fa0a73fbb
BuildTools <unconfigured@null.spigotmc.org>
2022-03-13 19:07:17 +0100

    a

--- PlanetFinder.lua
+++ PlanetFinder.lua
@@ -1,22 +1,16 @@
 ---@diagnostic disable: undefined-global
-local Screen = GetPartFromPort(1, 'Screen') -- assigning the screen on port 1 to a variable
-Screen:ClearElements() -- clear all elements already on the screen so there arent any old elements on it
-local TextLabel = Screen:CreateElement('TextLabel', {
-    AnchorPoint = Vector2.new(0, 0);
+local starmap = GetPartFromPort(1, 'Starmap')
+local telescope = GetPartFromPort(2, 'Telescope')
+local screen = GetPartFromPort(3, 'Screen')
+
+screen:ClearElements()
+screen:CreateElement('TextLabel', {
+    AnchorPoint = Vector2.new(0.5, 0.5);
     Position = UDim2.fromScale(0.5, 0.5);
     Size = UDim2.fromScale(1, 1);
-    Text = 'Hello, world!';
+    --Text = 'Hello, world!';
+    Text = JSONEncode(starmap:GetBodies());
     TextSize = 20;
     TextScaled = false;
 })
 
-wait(5)
-
-TextLabel:ChangeProperties({ -- change the text and size
-    Text = 'Goodbye, world!';
-    TextSize = 25
-})
-
-wait(2.5)
-
-Screen:ClearElements() -- remove everything from the screen
