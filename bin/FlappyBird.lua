7d69d97fde8577a6a8b6bb61ffe7b22881e4c8a6
BuildTools <unconfigured@null.spigotmc.org>
2022-03-19 00:19:06 +0100

    a

--- bin/FlappyBird.lua
+++ bin/FlappyBird.lua
@@ -393,5 +393,5 @@ end
 local screen = GetPartFromPort(1, 'Screen')
 
 canvas.prepare(screen, 150, 150, 1)
-canvas.polygon(0, 0, 0, 0, 25*3-1, 0, 25*3-1, 25*3-1, 0, 25*3-1)
+canvas.polygon(0, 0, 0, 0, 25*2-1, 0, 25*2-1, 25*2-1, 0, 25*2-1)
 canvas.present()
